da264f20b6f7a97d1976c25385cc342181447ea0
Pavel Voronov <pavelvoronov63@gmail.com>
2023-08-02 23:59:19 +0300

    Добавил шаблон программы

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Start" << std::endl;
+
+    std::string inp;
+    std::cin >> inp;
+    std::cout << inp;
+
+    std::cout << "End" << std::endl;
+    return 0;
+}
